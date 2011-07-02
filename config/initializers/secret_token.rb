f700ce634160b71a9a27f442a9d13aa7ba513e14
unknown <rob@.(none)>
2011-07-02 16:11:18 -0400

    CompanyLook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompanyLook::Application.config.secret_token = '90b62e18b726298067bc243a880fd9153a9855d31c711237f4d00eeef02081a55949c3c6c0a17a49e199f12774ae08ca04d94ae11ae3baff3ef4a5a1f33e3ddb'
