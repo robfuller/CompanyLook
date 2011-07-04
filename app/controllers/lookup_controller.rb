578162db49c7954b58bbd909b14b767693471228
Rob Fuller <robfuller@gmail.com>
2011-07-04 15:41:22 -0400

    trying to make search work

--- app/controllers/lookup_controller.rb
+++ app/controllers/lookup_controller.rb
@@ -1,2 +1,15 @@
 class LookupController < ApplicationController
+
+	def index
+	end
+
+	def create
+		 query = params[:search]
+		 url = 'http://api.crunchbase.com/v/1/search.js?query=' + query
+		 @results = json_parse(url)
+	end
+
+	def show
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ CompanyLookup::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :lookup 
+
+
 
   # Sample resource route with options:
   #   resources :products do
@@ -48,7 +50,7 @@ CompanyLookup::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'lookup#index'
 
   # See how all your routes lay out with "rake routes"
 
