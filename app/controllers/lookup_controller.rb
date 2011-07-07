81153000d5befe0b3ce371453873b98513ebd8fb
Rob Fuller <robfuller@gmail.com>
2011-07-06 21:07:42 -0400

    oh yeah! JSONP in tha house

--- app/controllers/lookup_controller.rb
+++ app/controllers/lookup_controller.rb
@@ -1,15 +1,25 @@
+require 'json'
+
 class LookupController < ApplicationController
 
 	def index
-	end
-
-	def create
-		 query = params[:search]
+		 query = params[:query]
 		 url = 'http://api.crunchbase.com/v/1/search.js?query=' + query
 		 @results = json_parse(url)
+		 render :json => @results, :callback => params[:callback] 
 	end
 
 	def show
 	end
 
+	def json_parse(url) 
+		uri = URI.parse(url)
+		result = Net::HTTP.get_response(uri)
+		data = result.body
+		result = JSON.parse(data)
+		if result.has_key? "error"
+			raise "error:" + result["error"]
+		end
+		return data 
+	end
 end
